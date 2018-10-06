23a2bde4acbb9cf4b9ee09bc9af22da6179fcb73
Sung Bae <singsang2@gmail.com>
2018-10-06 03:56:10 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,13 +1,11 @@
 
 def levitation_quiz
-	puts "What is the spell that enacts levitation?"
-	spell = gets.strip
-	if spell == "Wingardium Leviosa" 
-	  puts "You passed the quiz!"
-	  break
-	else 
-	  leviation_quiz
-	end
+	loop do
+  	puts "What is the spell that enacts levitation?"
+  	spell = gets.strip
+  	
+  	break if spell == "Wingardium Leviosa"
+  end
 end
 
 
